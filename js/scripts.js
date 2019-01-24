5403d7d564c87db4bad43b893f04f1ca703cdde9
Micaela Jawor and Tanner Damron <tanner@>
2019-01-24 08:39:33 -0800

    add html, create frontend js, and first argument to js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//business-logic
+var leapYear = function(year) {
+  return false;
+};
+//UI
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
